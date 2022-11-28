89e40cbe93bd5f054955039afcfc66dd5523508e
Cris <criguzmon@gmail.com>
2022-11-28 19:57:27 +0100

    Se realizan tests de validación de generación de usuario

--- Dominio/Usuario.cs
+++ Dominio/Usuario.cs
@@ -10,7 +10,26 @@ public class Usuario
 
     public static Result<Usuario> Generar(string nombre, string apellidos, string email, string contraseña, int? idUsuario=null)
     {
-        return null;
+        if (string.IsNullOrEmpty(nombre)) return Result.Fail<Usuario>("El nombre no puede ser nulo ni estar vacío");
+        if (string.IsNullOrEmpty(apellidos)) return Result.Fail<Usuario>("Los apellidos no puede ser nulo ni estar vacío");
+        if (string.IsNullOrEmpty(email)) return Result.Fail<Usuario>("El email no puede ser nulo ni estar vacío");
+        if (string.IsNullOrEmpty(contraseña)) return Result.Fail<Usuario>("La contraseña no puede ser nulo ni estar vacío");
+
+        Usuario usuario = new Usuario(nombre, apellidos, email, contraseña, idUsuario);
+
+        return Result.Ok(usuario);
+    }
+
+    protected Usuario(string nombre, string apellidos, string email, string contraseña, int? idUsuario = null)
+    {
+        if(idUsuario != null) IdUsuario = idUsuario.Value;
+
+        Nombre = nombre;
+        Apellidos = apellidos;
+        Email = email;
+        Contraseña = contraseña; 
+
+
     }
 
 }
--- TestUnitario/TestUsuario.cs
+++ TestUnitario/TestUsuario.cs
@@ -4,15 +4,17 @@ namespace TestUnitario;
 
 public class TestUsuario
 {
+
+    string nombre = "Cris";
+    string apellido = "Guzman";
+    string email = "cristina.guzman@prueba.es";
+    string contraseña = "12345";
+
     [Fact]
     public void TestGenerar_TodosLosParametrosObligatorios_Exito()
     {
         //Arrange-Given
 
-        string nombre = "Cristina";
-        string apellido = "Guzman";
-        string email = "cristina.guzman@prueba.es";
-        string contraseña = "12345";
 
         //Act-When
 
@@ -22,6 +24,140 @@ public class TestUsuario
 
         Assert.True(resultado.IsSuccess);
 
-        
     }
+
+    [Fact]
+    public void TestGenerar_NombreVacio_Exito()
+    {
+        //Arrange-Given
+
+        string nombre = "";
+
+        //Act-When
+
+        Result<Usuario> resultado = Usuario.Generar(nombre, apellido, email, contraseña);
+
+        //Assert-Then
+
+        Assert.False(resultado.IsSuccess);
+        Assert.Equal("El nombre no puede ser nulo ni estar vacío", resultado.Error);
+
+    }
+
+    [Fact]
+    public void TestGenerar_NombreNull_Exito()
+    {
+        //Arrange-Given
+
+        //Act-When
+
+        Result<Usuario> resultado = Usuario.Generar(null, apellido, email, contraseña);
+
+        //Assert-Then
+
+        Assert.False(resultado.IsSuccess);
+        Assert.Equal("El nombre no puede ser nulo ni estar vacío", resultado.Error);
+
+    }
+
+    [Fact]
+    public void TestGenerar_ApellidosVacio_Exito()
+    {
+        //Arrange-Given
+
+
+        //Act-When
+
+        Result<Usuario> resultado = Usuario.Generar(nombre, "", email, contraseña);
+
+        //Assert-Then
+
+        Assert.False(resultado.IsSuccess);
+        Assert.Equal("Los apellidos no puede ser nulo ni estar vacío", resultado.Error);
+
+    }
+
+    [Fact]
+    public void TestGenerar_ApellidosNull_Exito()
+    {
+        //Arrange-Given
+
+        //Act-When
+
+        Result<Usuario> resultado = Usuario.Generar(nombre, null, email, contraseña);
+
+        //Assert-Then
+
+        Assert.False(resultado.IsSuccess);
+        Assert.Equal("Los apellidos no puede ser nulo ni estar vacío", resultado.Error);
+
+    }
+
+    [Fact]
+    public void TestGenerar_EmailVacio_Exito()
+    {
+        //Arrange-Given
+
+
+        //Act-When
+
+        Result<Usuario> resultado = Usuario.Generar(nombre, apellido, "", contraseña);
+
+        //Assert-Then
+
+        Assert.False(resultado.IsSuccess);
+        Assert.Equal("El email no puede ser nulo ni estar vacío", resultado.Error);
+
+    }
+
+    [Fact]
+    public void TestGenerar_EmailNull_Exito()
+    {
+        //Arrange-Given
+
+        //Act-When
+
+        Result<Usuario> resultado = Usuario.Generar(nombre, apellido, null, contraseña);
+
+        //Assert-Then
+
+        Assert.False(resultado.IsSuccess);
+        Assert.Equal("El email no puede ser nulo ni estar vacío", resultado.Error);
+
+    }
+
+    [Fact]
+    public void TestGenerar_ContraseñaVacio_Exito()
+    {
+        //Arrange-Given
+
+
+        //Act-When
+
+        Result<Usuario> resultado = Usuario.Generar(nombre, apellido, email, "");
+
+        //Assert-Then
+
+        Assert.False(resultado.IsSuccess);
+        Assert.Equal("La contraseña no puede ser nulo ni estar vacío", resultado.Error);
+
+    }
+
+    [Fact]
+    public void TestGenerar_ContraseñaNull_Exito()
+    {
+        //Arrange-Given
+
+        //Act-When
+
+        Result<Usuario> resultado = Usuario.Generar(nombre, apellido, email, null);
+
+        //Assert-Then
+
+        Assert.False(resultado.IsSuccess);
+        Assert.Equal("La contraseña no puede ser nulo ni estar vacío", resultado.Error);
+
+    }
+
+
 }
